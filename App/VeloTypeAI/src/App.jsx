b9d0ef4c27a2e7f859b49bec587a09fe6938c80a
Kirit Ranjan <kiritranjan7@gmail.com>
2026-03-16 20:38:55 +0530

    Reduced code using ternary operators

--- App/VeloTypeAI/src/App.jsx
+++ App/VeloTypeAI/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
+
 import Login from './components/login.jsx'
 import Contact from './components/contact.jsx'
 import Security from './components/security.jsx'
@@ -18,23 +18,12 @@ function App() {
     setUI((oldUI) => 'security');
   }
 
-  let actual_ui;
-  if(ui=='login')
-  {
-    actual_ui = <Login contact_ui_changer={contact_ui_changer} security_ui_changer={security_ui_changer}/>
-  }
-  else if(ui=='contact')
-  {
-    actual_ui = <Contact />
-  }
-  else if(ui=='security')
-  {
-    actual_ui = <Security />
-  }
-
+  
   return (
     <>
-      {actual_ui}
+      {ui=="login"? <Login contact_ui_changer={contact_ui_changer} security_ui_changer={security_ui_changer}/> : null}
+      {ui=="contact"? <Contact /> : null}
+      {ui=="security"? <Security /> : null}
     </>
   )
 }
