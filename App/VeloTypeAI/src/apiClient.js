308b3470a531191eaa24ad1e0765fb33796466b5
vishnu97770 <vv162370@gmail.com>
2026-03-23 22:42:41 +0530

    Injected code for apiClient.js

--- App/VeloTypeAI/src/apiClient.js
+++ App/VeloTypeAI/src/apiClient.js
@@ -1,1 +1,22 @@
 const BASE_URL = "http://localhost:8000/api/v1"
+const apiClient = async (endpoint, options = {}) => {
+  const token = localStorage.getItem("access_token");
+
+  const res = await fetch(`${BASE_URL}${endpoint}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+    credentials: "include", // needed for httpOnly refresh token cookie
+    ...options,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.detail || "Something went wrong");
+  }
+
+  return res.json();
+};
+
+export default apiClient;
