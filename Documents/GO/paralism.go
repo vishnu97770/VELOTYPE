8a8468f9839ea0294277316401537e774d0f6a7e
vishnu97770 <vv162370@gmail.com>
2026-08-04 14:14:12 +0530

    Variables

---
+++ Documents/GO/paralism.go
@@ -0,0 +1,3 @@
+// what is the concept of paralism and concurrency? 
+// what is the difference between golang and other programming language 
+// what is the difference between package and module 
---
+++ Documents/GO/variables.go
@@ -0,0 +1,9 @@
+#Variables 
+
+package main
+import "fmt"
+func main(){
+	age := 20
+	name := cris
+	fmt.Println(age, name)
+}
